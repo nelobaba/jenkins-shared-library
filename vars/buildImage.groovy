a5a6a6865858eaba284ede42ebdbe547005cb2bd
Nelson Umunna <nelson.umunna@carleton.ca>
2022-04-27 14:30:03 -0400

    Updated

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,11 @@
 #!/usr/bin/env groovy
-import com.example.Docker
 
 def call(String imageName){
-    return new Docker(this).buildDockerImage(imageName)
+    // return new Docker(this).buildDockerImage(imageName)
+     echo "building image"
+     withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+         sh "docker build -t $imageName ."
+         sh "echo $PASS | docker login -u $USER --password-stdin"
+         sh "docker push $imageName"
+     }
 }
